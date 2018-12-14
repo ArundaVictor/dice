3d6c837bb726b760fec1837fdb09ea7d514efcf5
ArundaVictor <victoryugi31@gmail.com>
2018-12-14 17:22:49 +0300

    make new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+
+  var x = function roll() {
+  return Math.floor((Math.random()*6)+1);
+}
+
+
 $(document).ready(function() {
-  $(".btn").click(function() {
+
+  $("#r1").click(function() {
+    $("#roll1").val(x);
+  });
+  $("#r2").click(function() {
+    $("#roll2").val(x);
+  });
+
+    $(".button").click(function() {
     $(".row").slideToggle();
+    $("#hold").slideToggle();
   });
 });
