4ae0713ead7a568ae5af121f774bfa893ca6cc86
ArundaVictor <victoryugi31@gmail.com>
2018-12-15 19:43:53 +0300

    seperate business logic and user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-var player1 = "";
-var player2 = "";
+
 var x = function () {
   return Math.floor(Math.random()*6)+1;
 }
@@ -8,14 +7,34 @@ this.roll = 0;
 this.tempscore = 0;
 this.totalscore = 0;
 this.turn = turn;
-this.playerName;
+this.current;
 }
-Player.prototype.roll1 = function() {
+Player.prototype.roll = function() {
   if (this.roll === 1) {
     this.tempscore = 0;
-    alert("Sorry "+this.playerName+ "you rolled a 1!It's Player's 2 turn now")
+    alert("Sorry "+this.current+ "you rolled a 1!It's Player's 2 turn now")
 
   }else {
+    this.tempscore += this.roll;
+  }
+  Player.prototype.hold = function() {
     this.totalscore += this.tempscore;
+    this.tempscore = 0;
+    alert(this.current + ", It's now the other player's turn");
+
+  }
+  Player.prototype.change = function() {
+    if (this.roll === 1) {
+      this.turn = false;
+
+    }else {
+      this.turm = true;
+    }
+  }
+  Player.prototype.win = function() {
+    if (this.totalscore>=100) {
+      alert(+this.current+ "Wins!!!")
+
+    }
   }
 }
