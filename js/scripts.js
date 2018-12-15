e62f0dc0c7eeb39c7153af7dd0219e4d5f636ed0
ArundaVictor <victoryugi31@gmail.com>
2018-12-15 19:19:00 +0300

    add new function and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
-
-  var x = function roll() {
-  return Math.floor((Math.random()*6)+1);
+var player1 = "";
+var player2 = "";
+var x = function () {
+  return Math.floor(Math.random()*6)+1;
 }
+function Player(turn) {
+this.roll = 0;
+this.tempscore = 0;
+this.totalscore = 0;
+this.turn = turn;
+this.playerName;
+}
+Player.prototype.roll1 = function() {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert("Sorry "+this.playerName+ "you rolled a 1!It's Player's 2 turn now")
 
-
-$(document).ready(function() {
-
-  $("#r1").click(function() {
-    $("#roll1").val(x);
-  });
-  $("#r2").click(function() {
-    $("#roll2").val(x);
-  });
-
-    $(".button").click(function() {
-    $(".row").slideToggle();
-    $("#hold").slideToggle();
-  });
-});
+  }else {
+    this.totalscore += this.tempscore;
+  }
+}
