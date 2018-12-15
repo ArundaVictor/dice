7ca128d550721884574cdba64a87fc2856e34348
ArundaVictor <victoryugi31@gmail.com>
2018-12-15 22:51:02 +0300

    add new function hold

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,32 @@
 
-var x = function () {
+var rollDice = function() {
   return Math.floor(Math.random()*6)+1;
 }
-function Player(turn) {
-this.roll = 0;
-this.tempscore = 0;
-this.totalscore = 0;
-this.turn = turn;
-this.current;
-}
-Player.prototype.roll = function() {
-  if (this.roll === 1) {
-    this.tempscore = 0;
-    alert("Sorry "+this.current+ "you rolled a 1!It's Player's 2 turn now")
+var hold = function() {
+  alert("It's the other player's turn");
 
-  }else {
-    this.tempscore += this.roll;
-  }
-  Player.prototype.hold = function() {
-    this.totalscore += this.tempscore;
-    this.tempscore = 0;
-    alert(this.current + ", It's now the other player's turn");
 
   }
-  Player.prototype.change = function() {
-    if (this.roll === 1) {
-      this.turn = false;
 
-    }else {
-      this.turm = true;
-    }
-  }
-  Player.prototype.win = function() {
-    if (this.totalscore>=100) {
-      alert(+this.current+ "Wins!!!")
 
-    }
-  }
-}
+
+
+
+$(document).ready(function() {
+
+  $("#r1").click(function() {
+    $("#roll1").val(rollDice);
+  });
+  $("#r2").click(function() {
+    $("#roll2").val(rollDice);
+  });
+  $(".btn").click(function() {
+    hold();
+
+  });
+
+    $(".button").click(function() {
+    $(".row").slideToggle();
+    $("#hold").slideToggle();
+  });
+});
