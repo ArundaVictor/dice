c0bd11aa82b70c4e3f335a9421ad55c9c5a2ee6e
ArundaVictor <victoryugi31@gmail.com>
2018-12-16 09:52:46 +0300

    make new changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,19 @@
 var rollDice = function() {
   return Math.floor(Math.random()*6)+1;
 }
+function newPlay() {
+  this.roll = 0;
+  this.totalScore = 0;
+}
+newPlay.prototype.roll1 = function() {
+  if (this.roll === 1) {
+  this.totalScore = 0;
+  alert("Sorry you rolled a 1! Your turn is over!");
+
+  } else {
+  this.totalScore += this.roll;
+  }
+}
 var hold = function() {
   alert("It's the other player's turn");
 
@@ -14,8 +27,10 @@ var hold = function() {
 
 $(document).ready(function() {
 
-  $("#r1").click(function() {
+  $("#r1").click(function(event) {
     $("#roll1").val(rollDice);
+    $("#score1").val(rollDice);
+
   });
   $("#r2").click(function() {
     $("#roll2").val(rollDice);
